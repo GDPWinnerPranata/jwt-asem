fdbda6f0833008d755361ee3b3aedafef466c12d
GDPWinnerPranata <winner.pranata@gdplabs.id>
2023-07-24 17:37:51 +0700

    memperpendek ehe

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ async function main() {
     randomPrivateKey
   );
   console.log({ jwt });
-  
+
   // Verifying
-  const address = ethers.utils.computeAddress("0x" + randomPrivateKey)
+  const address = ethers.utils.computeAddress("0x" + randomPrivateKey);
   console.log({ verify: await verifyJwt(jwt, address) });
 }
 
 export async function signToJwt(payload, privateKey) {
-  const { privateKeypair } = await keyPairFromPrivateKey(privateKey);
+  const privateKeypair = await getPrivateKeypair(privateKey);
 
   const jwt = await new jose.SignJWT(payload)
     .setProtectedHeader({ alg: "ES256K", typ: "JWT" })
@@ -45,36 +45,28 @@ export function verifyJwt(jwt, address) {
   );
 }
 
-async function keyPairFromPrivateKey(privateKey) {
+async function getPrivateKeypair(privateKey) {
   const c = new ec.ec("secp256k1");
   const publicKey = c.keyFromPrivate(privateKey).getPublic("hex");
   const publicX = publicKey.substring(2, 2 + 64);
   const publicY = publicKey.split(publicX)[1];
 
-  const publicJwk = {
+  const privateJwk = {
     kty: "EC",
     crv: "secp256k1",
     x: Buffer.from(publicX, "hex").toString("base64url"),
     y: Buffer.from(publicY, "hex").toString("base64url"),
+    d: Buffer.from(privateKey, "hex").toString("base64url"),
   };
 
-  const publicKeypair = await jose.importJWK(publicJwk);
-
-  const privateKeypair = await jose.importJWK({
-    ...publicJwk,
-    d: Buffer.from(privateKey, "hex").toString("base64url"),
-  });
-
-  return { privateKeypair, publicKeypair };
+  const privateKeypair = await jose.importJWK(privateJwk);
+  return privateKeypair;
 }
 
 function getPayloadDigest(jwt) {
   const jwtSplit = jwt.split(".");
-  const payload = Buffer.concat([
-    Buffer.from(jwtSplit[0], "utf-8"),
-    Buffer.from(".", "utf-8"),
-    Buffer.from(jwtSplit[1], "utf-8"),
-  ]);
+  
+  const payload = Buffer.from(jwtSplit[0] + "." + jwtSplit[1], "utf-8");
   const payloadDigest = crypto.createHash("sha256").update(payload).digest();
 
   const signature = Buffer.from(jwtSplit[2], "base64url");
